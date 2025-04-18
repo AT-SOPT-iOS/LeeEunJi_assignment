eed9d36a06bac4ce5ed9af29ba23abdfd97f074f
eunji <oieunxe@gmail.com>
2025-04-18 09:10:48 +0900

    프로젝트 생성

---
+++ SOPT_assignment/SOPT_assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOPT_assignment
+//
+//  Created by 이은지 on 4/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
