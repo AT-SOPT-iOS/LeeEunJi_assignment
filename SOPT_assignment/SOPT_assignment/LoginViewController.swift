c33516db4e7339eb32ddadb63ec816420d9d9d43
eunji <oieunxe@gmail.com>
2025-04-19 22:46:16 +0900

    feat: 아이디 찾기 버튼, 비밀번호 찾기 버튼 구현

--- SOPT_assignment/SOPT_assignment/LoginViewController.swift
+++ SOPT_assignment/SOPT_assignment/LoginViewController.swift
@@ -53,6 +53,30 @@ final class LoginViewController: UIViewController {
         button.addTarget(self, action: #selector(loginButtonDidTapped), for: .touchUpInside)
         return button
     }()
+    
+    private let findIdButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("아이디 찾기", for: .normal)
+        button.setTitleColor(UIColor.appColor(.gray2), for: .normal)
+        button.titleLabel?.font = UIFont.appFont(.pretendardSemiBold, size: 14)
+        button.addTarget(self, action: #selector(findIdButtonDidTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private let seperateView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.appColor(.gray4)
+        return view
+    }()
+    
+    private let findPasswordButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("비밀번호 찾기", for: .normal)
+        button.setTitleColor(UIColor.appColor(.gray2), for: .normal)
+        button.titleLabel?.font = UIFont.appFont(.pretendardSemiBold, size: 14)
+        button.addTarget(self, action: #selector(findPasswordButtonDidTapped), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,11 +87,19 @@ final class LoginViewController: UIViewController {
         print("")
     }
     
+    @objc private func findIdButtonDidTapped() {
+        print("아이디 찾기 버튼 눌림")
+    }
+    
+    @objc private func findPasswordButtonDidTapped() {
+        print("비밀번호 찾기 버튼 눌림")
+    }
+    
 }
 
 extension LoginViewController {
     private func setLayout() {
-        [loginInfoLabel, idTextField, passwordTextField, loginButton].forEach {
+        [loginInfoLabel, idTextField, passwordTextField, loginButton, findIdButton, seperateView, findPasswordButton].forEach {
             view.addSubview($0)
         }
     }
@@ -98,6 +130,27 @@ extension LoginViewController {
             make.trailing.equalToSuperview().offset(-20)
             make.top.equalTo(passwordTextField.snp.bottom).offset(21)
             make.height.equalTo(52)
+        }
+        
+        findIdButton.snp.makeConstraints { make in
+            make.trailing.equalTo(seperateView.snp.leading).offset(-33)
+            make.centerY.equalTo(seperateView.snp.centerY)
+            make.width.greaterThanOrEqualTo(62)
+            make.height.greaterThanOrEqualTo(22)
+        }
+        
+        seperateView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(loginButton.snp.bottom).offset(36)
+            make.width.equalTo(1)
+            make.height.equalTo(12)
+        }
+        
+        findPasswordButton.snp.makeConstraints { make in
+            make.leading.equalTo(seperateView.snp.trailing).offset(36)
+            make.centerY.equalTo(seperateView.snp.centerY)
+            make.width.greaterThanOrEqualTo(73)
+            make.height.greaterThanOrEqualTo(22)
         }
     }
 
