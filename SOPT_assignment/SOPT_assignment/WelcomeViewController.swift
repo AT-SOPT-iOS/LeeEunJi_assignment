7b856ed20c7e7aac1d261d7fb5eeb0d0460180cd
eunji <oieunxe@gmail.com>
2025-04-19 23:37:06 +0900

    feat: 티빙 로고 이미지 구현

--- SOPT_assignment/SOPT_assignment/WelcomeViewController.swift
+++ SOPT_assignment/SOPT_assignment/WelcomeViewController.swift
@@ -6,15 +6,41 @@
 //
 
 import UIKit
+import SnapKit
 
 class WelcomeViewController: UIViewController {
+    
+    private let tvingLogoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "tvingLogo")
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        configureView()
     }
 
 
 }
 
+extension WelcomeViewController {
+    private func setLayout() {
+        [tvingLogoImage].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setUpConstraints() {
+        tvingLogoImage.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+
+    private func configureView() {
+        view.backgroundColor = .black
+        setLayout()
+        setUpConstraints()
+    }
+}
