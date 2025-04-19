8c9905daaec4e3210595a44e7cc2be23c678f9af
eunji <oieunxe@gmail.com>
2025-04-19 14:51:42 +0900

    feat: TVING ID 로그인 라벨 구현

--- SOPT_assignment/SOPT_assignment/LoginViewController.swift
+++ SOPT_assignment/SOPT_assignment/LoginViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
 final class LoginViewController: UIViewController {
+    
+    private let loginInfoLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.appColor(.gray1)
+        label.textAlignment = .center
+        label.font = UIFont.appFont(.pretendardRegular, size: 23)
+        label.text = "TVING ID 로그인"
+        return label
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .black
+        configureView()
     }
 
 
+    
+}
+
+extension LoginViewController {
+    private func setLayout() {
+        [loginInfoLabel].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setUpConstraints() {
+        loginInfoLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(50)
+        }
+    }
+
+    private func configureView() {
+        self.view.backgroundColor = .black
+        setLayout()
+        setUpConstraints()
+    }
 }
