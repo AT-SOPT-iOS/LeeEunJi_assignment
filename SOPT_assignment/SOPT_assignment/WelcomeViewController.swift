b4b2c3052cd6178b2f0969436dc4e51a8c92393d
eunji <oieunxe@gmail.com>
2025-04-19 23:13:21 +0900

    feat: WelcomeViewController.swift 파일 생성

---
+++ SOPT_assignment/SOPT_assignment/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  SOPT_assignment
+//
+//  Created by 이은지 on 4/19/25.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
