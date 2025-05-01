5c2aa4806f6683c17a527b8bd2c6491787ab2b7c
eunji <oieunxe@gmail.com>
2025-05-01 11:33:41 +0900

    feat: MainViewcontrollero.swift 생성

---
+++ SOPT_assignment/SOPT_assignment/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  SOPT_assignment
+//
+//  Created by 이은지 on 5/1/25.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    }
+
+
+}
+
