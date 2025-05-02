fa98f5dc184f5a013fc745224da07797994fd21d
eunji <oieunxe@gmail.com>
2025-05-02 15:15:06 +0900

    feat: 상단 로고, 서치 아이콘, 이미지 추가

--- SOPT_assignment/SOPT_assignment/MainViewController.swift
+++ SOPT_assignment/SOPT_assignment/MainViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import SnapKit
 
 final class MainViewController: UIViewController {
+    
+    // MARK: - UI Components
+    private let scrollView = UIScrollView()
+    
+    private let tvingLogoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "mainTvingLogo")
+        return imageView
+    }()
+    
+    private let searchButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "search"), for: .normal)
+        return button
+    }()
+    
+    private let tvingSmallLogoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "smallTvingLogo")
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
+        setLayout()
+        setUpConstraints()
     }
 
 
 }
 
+extension MainViewController {
+    private func setLayout() {
+//        [scrollView].forEach {
+//            view.addSubview($0)
+//        }
+        
+        [tvingLogoImage, searchButton, tvingSmallLogoImage].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setUpConstraints() {
+//        scrollView.snp.makeConstraints {
+//            $0.top.equalToSuperview()
+//            $0.horizontalEdges.equalToSuperview()
+//            $0.bottom.equalTo(tvingSmallLogoImage.snp.bottom).offset(50)
+//        }
+        
+        tvingLogoImage.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(31)
+            $0.leading.equalToSuperview()
+            $0.height.greaterThanOrEqualTo(78)
+            $0.width.greaterThanOrEqualTo(191)
+        }
+        
+        searchButton.snp.makeConstraints {
+            $0.centerY.equalTo(tvingLogoImage.snp.centerY)
+            $0.trailing.equalTo(tvingSmallLogoImage.snp.leading).offset(-10)
+            $0.width.height.equalTo(30)
+        }
+        
+        tvingSmallLogoImage.snp.makeConstraints {
+            $0.centerY.equalTo(tvingLogoImage.snp.centerY)
+            $0.trailing.equalToSuperview().offset(-11)
+            $0.width.height.equalTo(30)
+        }
+    }
+}
