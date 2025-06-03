444f7b975fdb88551e8b5743b92218334a744f10
eunji <oieunxe@gmail.com>
2025-06-03 22:22:54 +0900

    feat: 상단 카테고리 헤더 뷰 및 하단 공지 푸터 뷰 구현

--- SOPT_assignment/SOPT_assignment/Week7/MainView.swift
+++ SOPT_assignment/SOPT_assignment/Week7/MainView.swift
@@ -5,4 +5,63 @@
 //  Created by 이은지 on 6/3/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct MainView: View {
+    @State private var selectedCategoryIndex: Int = 0
+
+    private let categories: [Category] = [
+        Category(name: "홈"),
+        Category(name: "드라마"),
+        Category(name: "예능"),
+        Category(name: "영화"),
+        Category(name: "스포츠"),
+        Category(name: "뉴스")
+    ]
+    
+    
+    private var headerView: some View {
+        HStack {
+            Image("mainTvingLogo")
+            Spacer()
+            Button(action: {}) {
+                Image("search")
+            }
+            Image("smallTvingLogo")
+        }
+        .padding(.horizontal)
+    }
+
+    
+    private var noticeView: some View {
+        HStack {
+            Text("공지")
+                .font(.custom("Pretendard-Medium", size: 11))
+                .foregroundColor(Color(UIColor.appColor(.tvingNotice)))
+            Text("티빙 계정 공유 정책 추가 안내")
+                .font(.custom("Pretendard-Medium", size: 11))
+                .foregroundColor(Color(UIColor.appColor(.tvingNoticeTitle)))
+            Spacer()
+            Image("goToNotice")
+        }
+        .padding()
+        .background(Color(UIColor.appColor(.tvingAnotherLogo)))
+        .cornerRadius(5)
+        .padding(.horizontal, 14)
+    }
+
+    private var footerView: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("고객문의 · 이용약관 · 개인정보처리방침")
+            Text("사업자정보 · 인재채용")
+        }
+        .font(.custom("Pretendard-Medium", size: 11))
+        .foregroundColor(.gray)
+        .padding(.horizontal, 20)
+        .padding(.bottom, 100)
+    }
+}
+
+#Preview {
+    MainView()
+}
